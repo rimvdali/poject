7b87902446e221206a6f84a89a63ff0a9c780366
Vladimir <vladimir@uskmari.ru>
2020-12-20 19:49:47 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+let incr = 10,
+    decr = 10;
+
+//++incr;
+//--decr;
+
+console.log(2 + 2 * 2 !== '6');
+
+const isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
+
